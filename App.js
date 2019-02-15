53521772449115b38cdf6450f2969eb4c96d8ddd
Riddhi <Riddhi@bash.local>
2019-02-15 18:09:23 +0530

    senddata

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View , Image } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, Image } from 'react-native';
 
 export default class FetchExample extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={ isLoading: true}
+    this.state = { isLoading: true }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     return fetch('https://reqres.in/api/users/2')
       .then((response) => response.json())
       .then((responseJson) => {
@@ -16,38 +16,34 @@ export default class FetchExample extends React.Component {
         this.setState({
           isLoading: false,
           dataSource: responseJson,
-        }, function(){
+        }, function () {
 
         });
 
       })
-      .catch((error) =>{
+      .catch((error) => {
         console.error(error);
       });
   }
-  render(){
-    const {dataSource}=this.state
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
+  render() {
+    const { dataSource } = this.state
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, marginTop: 200 }}>
+          <ActivityIndicator size="large" color="#00ff00"/>
         </View>
       )
     }
 
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
+    return (
+      <View style={{ flex: 1, marginTop: 100 }}>
+
+        <Text style={{color:'white',fontSize:40}}>{ dataSource.data.first_name} {dataSource.data.last_name}  </Text>
+        <Image
+          source={{ uri: `${dataSource.data.avatar}` }}
+          style={{ height: 100, width: 100 }}
+        />
         {/* <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item}</Text>}
-          keyExtractor={({id}, index) => id}
-        /> */}
-        {/* <Text>{dataSource.data.first_name} {dataSource.data.last_name}  </Text>
-        <Image 
-        source={{uri: `${dataSource.data.avatar}`}}
-          style={{height:100,width:100}}
-        /> */}
-          {/* <FlatList
           data={this.state.dataSource}
           renderItem={({item}) => <Text>{item}</Text>}
           keyExtractor={({id}, index) => id}
